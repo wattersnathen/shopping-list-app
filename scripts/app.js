16bb36f343fdebd465eb78d7255c668400d5d423
Nathen Watters <wattersnathen@gmail.com>
2014-12-05 12:14:40 -0700

    added jquery-ui library, added color pallete, header, removed scripts for now

--- scripts/app.js
+++ scripts/app.js
@@ -1,44 +1,5 @@
-$(document).ready(function() {
-  
-  var itemToAdd = $("#enter-item");
+$(document).ready(function(){
 
-  $("#add-item-button").click(function() {
-    addItemToList(itemToAdd);
-  });
 
-  $("#enter-item").keypress(function(event) {
-    if (event.which === 13) {
-      addItemToList(itemToAdd);
-    }
-  });
-
-  $("#item-list ul").on("click", "button", function() {
-    $(this).closest("li").remove(); // remove whole list item from DOM when delete is clicked
-  });
-
-  $("#item-list ul").on("click", "li", function() {
-    $(this).find("input").toggleClass("purchased-item");
-  });
-
-  $(document).on("click", function() { // clicking anywhere on the document should add disabled back
-    $(this).find("#item-list").find("input").attr("disabled", true);
-  });
-
-  $("#item-list ul").on("dblclick", "li", function() {
-    $(this).find("input").removeAttr("disabled");
-  });
-
-  function addItemToList(item) {
-    var itemVal = item.val().trim();
-    if (itemVal) {
-      $("#error-message").hide();
-      var html = "<li><input type='text' value='" + itemVal + "' disabled><button class='delete'>Delete</button></li>";
-      $("#item-list ul").append(html);
-      item.val("");
-    } else {
-      $("#error-message").show();
-    }
-    item.focus();
-  }
 
 });
