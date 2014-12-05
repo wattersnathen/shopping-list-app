919cfc562d6577bea7177a3eb0516e829a02197f
Nathen Watters <wattersnathen@gmail.com>
2014-12-05 16:28:58 -0700

    grab data from text and number inputs and add item to 'to-purchase' list

--- scripts/app.js
+++ scripts/app.js
@@ -3,18 +3,21 @@ $(document).ready(function(){
   var listItem = "<li class='item'>" +
                  "<input type='checkbox'>" +
                  "<input type='text' value='%data%'>" +
-                 "<input type='number' min='1'>" +
-                 "<input type='button' value='Edit'>" +
-                 "<input type='button' value='Delete'>" +
+                 "<input type='number' min='1' value='%qty%'>" +
+                 "<input type='button' value='Edit' class='button-edit'>" +
+                 "<input type='button' value='Delete' class='button-delete'>" +
                  "</li>",
       itemsList = $("#items"),          // list of items currently left to purchase
       purchasedList = $("#purchased");  // list of items that have been purchased
 
-  function addItemToList(item, list) {
-    var itemValue = item.val().trim();
-    if (itemValue) {
-      list.append(listItem.replace("%data%", itemValue));
+  function addItemToList(item, quantity, list) {
+    var _item = item.val().trim(),
+        _qty  = quantity.val();
+    if(_item) {
+      list.append(listItem.replace("%data%", _item)
+                          .replace("%qty%", _qty));
       item.val("");
+      quantity.val("1");
     } else {
       alert("nothing to add");
     }
@@ -22,7 +25,7 @@ $(document).ready(function(){
   }
 
   $("#add-button").click(function() {
-    addItemToList($("#enter-item input[type='text']"), itemsList);
+    addItemToList($("#entered-text"), $("#entered-quantity"), itemsList);
   });
 
 });
