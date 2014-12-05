8600375b8c6016eff1d34c502688d27b4e10f478
Nathen Watters <wattersnathen@gmail.com>
2014-12-05 15:48:39 -0700

    changed height:100% to min-height:100% in container so it always adjusts to adding/removing dom elements, added addtolist functionality in js

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,28 @@
 $(document).ready(function(){
 
+  var listItem = "<li class='item'>" +
+                 "<input type='checkbox'>" +
+                 "<input type='text' value='%data%'>" +
+                 "<input type='number' min='1'>" +
+                 "<input type='button' value='Edit'>" +
+                 "<input type='button' value='Delete'>" +
+                 "</li>",
+      itemsList = $("#items"),          // list of items currently left to purchase
+      purchasedList = $("#purchased");  // list of items that have been purchased
 
+  function addItemToList(item, list) {
+    var itemValue = item.val().trim();
+    if (itemValue) {
+      list.append(listItem.replace("%data%", itemValue));
+      item.val("");
+    } else {
+      alert("nothing to add");
+    }
+    item.focus();
+  }
+
+  $("#add-button").click(function() {
+    addItemToList($("#enter-item input[type='text']"), itemsList);
+  });
 
 });
