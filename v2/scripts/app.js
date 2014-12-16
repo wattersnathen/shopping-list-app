b85a8db4827892778fa8a8f1aab79cbf14c8ebf4
Nathen Watters <wattersnathen@gmail.com>
2014-12-15 17:02:00 -0700

    checkbox functionality, input styling

--- v2/scripts/app.js
+++ v2/scripts/app.js
@@ -1,19 +1,48 @@
-var APP = (function() {
+$(document).ready(function() {
 
-  var item =  "<li class='item'>" + 
+  var item =  "<li class='item'>" +           // the 'item' <li> to add to the DOM
               "<input type='checkbox'>" +
-              "<input type='text'>" +
-              "<input type='number' min='1'>" +
+              "<input type='text' value='%data%' disabled='disabled'>" +
+              "<input type='number' min='1' value='%qty%' disabled='disabled'>" +
               "<input type='button' value='Edit'>" +
               "<input type='button' value='Delete'>" +
-              "</li>";
+              "</li>",
+      itemsOnList = $("#items-to-purchase"),  // list of items yet to be purchased (not checked off)
+      purchasedList = $("#items-purchased");  // list of items already purchased (checked off)
 
-  function addItem(listToAddTo) {
+  // Add item to itemsOnList when +Add is clicked...
+  $("#add-button").on("click", function addItemOnClick() {
+    var itemToAdd = $("#entered-item-text"),
+        itemQty   = $("#entered-item-quantity"),
+        trimedItem= itemToAdd.val().trim(),
+        trimedQty = itemQty.val();
 
-  }
+    if (trimedItem) {
+      itemsOnList.append(item.replace("%data%", trimedItem).replace("%qty%", trimedQty));
 
-  function removeItem(listToRemoveFrom) {
-    
-  }
+      // clear out the data in the entered item fields
+      itemToAdd.val("");
+      itemQty.val("1");
+    } else { // no text was entered into the text field
+      alert("Nothing to add!");
+    }
+
+    itemToAdd.focus();
+
+  }); // end of #add-button click handler
+
+
+  // Add item to purchased list when checkbox is clicked
+  $(".items").on("click", "input[type='checkbox']", function changeList() {
+    var item = $(this).closest("li"),
+        listID = item.closest("ul").attr("id");
+
+    if(listID === "items-purchased") {
+      $("#items-to-purchase").append(item);
+
+    } else if (listID === "items-to-purchase") {
+      $("#items-purchased").append(item);
+    }
+  });
 
 });
